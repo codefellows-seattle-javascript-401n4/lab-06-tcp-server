c6b5c1f4e84b8322494919afe46615697e7c36f3
T.Jay Rocket <tjayrocket@gmail.com>
2017-10-05 01:37:09 -0700

    Logging messages, the quit command crashes the server

--- lab-tjay/index.js
+++ lab-tjay/index.js
@@ -15,17 +15,36 @@ server.on('connection', (socket) => {
 
   console.log(`${user.nickname} has joined the chat.`);
 
-
-
   clientPool = [...clientPool, socket];
 
-
+  socket.on('data', (buffer) => {
+    
+    command.nickname();
+      return;
+    });
 
   socket.on('data', (buffer) => {
     let data = buffer.toString();
-    if(data.startsWith('/nickname')){
-      socket.nickname = data.split('/nickname')[1] || socket.nickname;
-      socket.write(`you are now known as ${socket.nickname}`);
+    if(data.startsWith('@quit')){
+      socket.write(`${socket.nickname} left the chat`);
+      socket.end();
+      return;
+    }
+  });
+
+  socket.on('data', (buffer) => {
+    let data = buffer.toString();
+    if(data.startsWith('')){
+      console.log(data);
+      socket.write(data);
+      return;
+    }
+  });
+
+  socket.on('data', (buffer) => {
+    let data = buffer.toString();
+    if(data.startsWith('@list')){
+      // socket.write(clientPool);
       return;
     }
   });
--- lab-tjay/lib/commands.js
+++ lab-tjay/lib/commands.js
@@ -1,1 +1,16 @@
+// 'use strict';
 
+// let command = module.exports = {};
+
+// const net = require('net');
+
+// command.nickname = function(){
+//     socket.on('data', (buffer) => {
+//     let data = buffer.toString();
+//     if(data.startsWith('@nickname')){
+//       socket.nickname = data.split('@nickname')[1] || socket.nickname;
+//       socket.write(`You are now known as ${socket.nickname}`);
+//       return;
+//     }
+//   })
+// };
