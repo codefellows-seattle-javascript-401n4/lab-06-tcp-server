533a5721957c05e8c0ad9a369f84a0a847c5899a
Ashkaan Jaberi <ashkaan14@gmail.com>
2017-10-09 16:08:57 -0700

    lab done

--- index.js
+++ index.js
@@ -15,14 +15,7 @@ server.on('connection', (socket) => {
     controller.data(buffer, socket);
   });
 
-
 });
 
 
-
 server.listen(3000, () => console.log('server up!'));
-
-
-
-
-// handle the .error and .disconnect - console.log who left, show list of new users
